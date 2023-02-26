f8db1fc54ebe8ab5294152e1a943e3c37ac0f909
unknown <mikesantory590@gmail.com>
2023-02-26 17:06:05 -0600

    segundo commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 //aqui se mostrara  un texto que dice hola mundo
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div> BIENVENIDO A NEXTJS OMAR!</div>
   }
   
   export default HomePage
